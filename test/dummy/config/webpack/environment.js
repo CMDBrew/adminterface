362a37e1787aa54a7ea94a211572976e4f6394e6
I-Lung Lee <ilung@hey.com>
2021-07-13 11:49:38 -0700

    Remove jquery and @activeadmin/activeadmin js dependencies completely

--- test/dummy/config/webpack/environment.js
+++ test/dummy/config/webpack/environment.js
@@ -1,9 +1,7 @@
 const { environment } = require('@rails/webpacker')
-const jquery = require('./plugins/jquery')
 
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader'
 })
 
-environment.plugins.prepend('jquery', jquery)
 module.exports = environment
