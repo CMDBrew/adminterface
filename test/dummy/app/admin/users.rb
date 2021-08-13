ffc6ec0457462ad2f699d7db20f2fb55f5d66846
I-Lung Lee <ilung@hey.com>
2021-08-13 10:01:00 -0700

    Add trix as a dependency and add stylesheets

--- test/dummy/app/admin/users.rb
+++ test/dummy/app/admin/users.rb
@@ -86,7 +86,7 @@ ActiveAdmin.register User do
         end
 
         f.input :password, input_html: {autocomplete: "new-password"}
-        f.input :biography, counter: true
+        f.input :biography, as: :rich_text
         f.has_many :user_addresses, allow_destroy: true, sortable: :position, sortable_start: 1 do |k|
           k.inputs(class: "row") do
             k.input :fullname, wrapper_html: {class: "col-lg-6"}
