840fa326ae423f0bfcdb6eabd20f20aed5cd7669
I-Lung Lee <ilung@hey.com>
2021-09-11 11:42:21 -0700

    refactor: remove unnecessary bootstrap imports

--- app/javascript/adminterface/initializers/bootstrap.js
+++ app/javascript/adminterface/initializers/bootstrap.js
@@ -1,12 +1,12 @@
 /* global adminterface */
 
-import * as Bootstrap from 'bootstrap'
+import { Popover, Tooltip } from 'bootstrap'
 
 const BootstrapInit = function (element) {
   const popoverTriggerList = [].slice.call(element.querySelectorAll('[data-bs-toggle="popover"]'))
   const tooltipTriggerList = [].slice.call(element.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  const popoverInstances = popoverTriggerList.map((el) => new Bootstrap.Popover(el))
-  const tooltipInstances = tooltipTriggerList.map((el) => new Bootstrap.Tooltip(el))
+  const popoverInstances = popoverTriggerList.map((el) => new Popover(el))
+  const tooltipInstances = tooltipTriggerList.map((el) => new Tooltip(el))
 
   adminterface.popover = [...adminterface.popover, ...popoverInstances]
   adminterface.tooltip = [...adminterface.tooltip, ...tooltipInstances]
