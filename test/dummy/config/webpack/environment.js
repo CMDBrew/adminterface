6be95e3974279e21a778e4c53f585610baec328c
I-Lung Lee <ilung@hey.com>
2021-11-01 16:11:44 -0700

    feat: RTL support

--- test/dummy/config/webpack/environment.js
+++ test/dummy/config/webpack/environment.js
@@ -1,5 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
+// @TODO: hotfix for: ValidationError: Invalid options object. PostCSS Loader has been initialized using an options object that does not match the API schema
+// https://github.com/rails/webpacker/issues/2784#issuecomment-730636299
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName)
+  loader.use.forEach(loaderConfig => {
+    if (loaderConfig.options && loaderConfig.options.config) {
+      loaderConfig.options.postcssOptions = loaderConfig.options.config
+      delete loaderConfig.options.config
+    }
+  })
+})
+
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader'
 })
--- test/lib/configurations_test.rb
+++ test/lib/configurations_test.rb
@@ -9,6 +9,10 @@ module ConfigurationsTest
   end
 
   class DefaultTest < Base
+    test "default lang_dir" do
+      assert_equal "ltr", @application.lang_dir
+    end
+
     test "default components" do
       assert_equal @configs[:components], @application.components
     end
@@ -20,6 +24,7 @@ module ConfigurationsTest
 
   class GlobalTest < Base
     setup do
+      @application.lang_dir = "rtl"
       @application.components = {
         comments: {input: "string"},
         action_items: {new: {icon_class: "fake_icon"}, destroy: {only: %w[show edit]}}
@@ -28,6 +33,7 @@ module ConfigurationsTest
     end
 
     test "updates components" do
+      assert_equal("rtl", @application.lang_dir)
       assert_equal({input: "string"}, @application.components[:comments].deep_symbolize_keys)
       assert_equal(
         {
@@ -47,6 +53,7 @@ module ConfigurationsTest
   class NamespaceTest < Base
     setup do
       @namespace = ActiveAdmin::Namespace.new(@application, :super_admin)
+      @namespace.lang_dir = "rtl"
       @namespace.components = {
         comments: {input: "string"},
         action_items: {new: {icon_class: "fake_icon"}, destroy: {only: %w[show edit]}}
@@ -55,11 +62,13 @@ module ConfigurationsTest
     end
 
     test "does not equal the gloabl configs" do
+      assert_not_equal @application.lang_dir, @namespace.lang_dir
       assert_not_equal @application.components, @namespace.components
       assert_not_equal @application.css_classes, @namespace.css_classes
     end
 
     test "updates components" do
+      assert_equal "rtl", @namespace.lang_dir
       assert_equal({input: "string"}, @namespace.components[:comments].deep_symbolize_keys)
       assert_equal(
         {
@@ -81,6 +90,7 @@ module ConfigurationsTest
       @namespace = ActiveAdmin::Namespace.new(@application, :admin)
       @resource =
         @namespace.register(User) do
+          config.lang_dir = "rtl"
           config.components = {
             comments: {input: "string"},
             action_items: {new: {icon_class: "fake_icon"}, destroy: {only: %w[show edit]}}
@@ -90,16 +100,19 @@ module ConfigurationsTest
     end
 
     test "does not equal the gloabl configs" do
+      assert_not_equal @application.lang_dir, @resource.lang_dir
       assert_not_equal @application.components, @resource.components
       assert_not_equal @application.css_classes, @resource.css_classes
     end
 
     test "does not equal the namespace configs" do
+      assert_not_equal @namespace.lang_dir, @resource.lang_dir
       assert_not_equal @namespace.components, @resource.components
       assert_not_equal @namespace.css_classes, @resource.css_classes
     end
 
     test "updates components" do
+      assert_equal "rtl", @resource.lang_dir
       assert_equal({input: "string"}, @resource.components[:comments].deep_symbolize_keys)
       assert_equal(
         {
