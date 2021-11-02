a94ffff50ea0f4d7b50bff0881b04aabadc8dfe9
I-Lung Lee <ilung@hey.com>
2021-11-02 14:06:08 -0700

    fix: add aria-expanded to navbar-toggler

--- test/lib/extensions/views/header_test.rb
+++ test/lib/extensions/views/header_test.rb
@@ -43,7 +43,7 @@ class HeaderTest < ActiveAdmin::BaseTestCase
           <div class="site_title navbar-brand">
             <div class="title">Awesome Admin</div>
           </div>
-          <button class="navbar-toggler" data-bs-target="#header-nav" data-bs-toggle="collapse">
+          <button class="navbar-toggler" data-bs-target="#header-nav" data-bs-toggle="collapse" aria-expanded="false">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div id="header-nav" class="collapse navbar-collapse">
